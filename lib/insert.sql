42fda993072976f258a22455e986fbae59b6c8c7
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-10-28 18:00:40 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "white", "cool", 0),
+(2,"Rowdy", 10, "M", "brown", "playful", 1),
+(3,"Tabitha", 6, "F", "black", "cuddly", 1),
+(4,"Sargeant Brown", 8, "M", "brown", "tough", 0),
+(5,"Melissa", 13, "F", "dark brown", "quiet", 1),
+(6,"Grinch", 2, "M", "grey", "tough", 1),
+(7,"Wendy", 6, "F", "blonde", "quiet", 1),
+(8,null, 20, "M", "black", "cool", 0);
